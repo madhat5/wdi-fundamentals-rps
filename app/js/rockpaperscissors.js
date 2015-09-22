39323fbfd789adc0d5a86d0a804bce6b89e5f4ee
madhat5 <davidmalary@gmail.com>
2015-09-22 10:34:23 -0400

    rps David update1, unit3 hw

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,27 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move == true) {
+        move = move;
+    } else if (move == null) {
+        move = getInput ();
+        return move;
+    } else {
+        move = getInput ();
+        return move;
+    }
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+     if (move == true) {
+        move = move;
+    } else if (move == null) {
+        move = randomPlay;
+        return move;
+    } else {
+        move = randomPlay;
+        return move;
+    }   
 }
 
 function getWinner(playerMove,computerMove) {
